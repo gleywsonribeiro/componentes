529b061fd42d9c7c33faba7c443de38419daeb0e
gleyw <gleyw@192.168.1.3>
2016-08-08 14:14:42 -0300

    Utilizando calendario e configurando em portugues

--- src/main/java/br/com/iconeinformatica/componentes/bean/PerfilUsuarioBean.java
+++ src/main/java/br/com/iconeinformatica/componentes/bean/PerfilUsuarioBean.java
@@ -9,6 +9,7 @@ import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
@@ -24,11 +25,25 @@ public class PerfilUsuarioBean implements Serializable {
     private String senha;
     private String nome;
     private String sobre;
+    private Date dataNascimento;
     
     public void atualizar() {
-//        System.out.println("Senha: " + this.senha);
+        System.out.println("Senha: " + this.senha);
         System.out.println("Sobre: " + this.sobre);
+        System.out.println("Data de Nascimento " + this.dataNascimento);
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Perfil Atualizado"));
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+    
+    public Date getDataHoje() {
+        return new Date();
+    }
+    
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
     }
 
     public String getNome() {
